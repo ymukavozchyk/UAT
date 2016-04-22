10b3455b0be414e822b2db43ab5ff7ccd27805f2
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-04-21 23:53:08 -0500

    Processhtml optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,14 +14,25 @@ module.exports = function (grunt) {
                     { src: 'css/tidy.css', dest: 'css/tidy.min.css' }
                 ]
             }
+        },
+        processhtml: {
+            dist: {
+                files: {
+                'index.html': ['dev/index.html'],
+                'contact.html': ['dev/contact.html'],
+                'information.html': ['dev/information.html'],
+                'request-information.html': ['dev/request-information.html']
+                }
+            }
         }
     });
 
     // Load the plugins
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
 
     // Default tasks.
-    grunt.registerTask('default', ['uncss', 'cssmin']);
+    grunt.registerTask('default', ['uncss', 'cssmin', 'processhtml']);
 
 };
